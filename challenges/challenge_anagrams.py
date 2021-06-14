8708891a14d56d29af07bbb3f045a95a338313bf
CristianoNunes <cristianonunes99@gmail.com>
2021-06-13 23:43:26 -0300

    Challenge 3 Resolved

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,5 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    str1 = ''.join(sorted(first_string))
+    str2 = ''.join(sorted(second_string))
+
+    return str1 == str2
