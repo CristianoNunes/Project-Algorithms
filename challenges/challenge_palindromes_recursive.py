23cf2f13197f53ac9d949d21d064f487edba72d7
CristianoNunes <cristianonunes99@gmail.com>
2021-06-13 23:29:16 -0300

    Challenge 2 Resolved

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,7 @@
-def is_palindrome_recursive(word, low, high):
-    """ Faça o código aqui. """
+def is_palindrome_recursive(word, little, big):
+    if ((not word) or (word[little] != word[big])):
+        return False
+    elif (little > big):
+        return True
+    else:
+        return is_palindrome_recursive(word, little + 1, big - 1)
