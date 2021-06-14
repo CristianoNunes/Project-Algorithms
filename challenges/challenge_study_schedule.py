bcd821f7056a11e740ea07a09829bcc9b5eaea4e
CristianoNunes <cristianonunes99@gmail.com>
2021-06-13 23:13:38 -0300

    Challenge 1 Resolved

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(start_time, end_time, target_time):
-    """ Faça o código aqui. """
+    if (not start_time or not target_time):
+        return 0
+
+    result = 0
+    countStudents = len(start_time)
+    for i in range(0, countStudents):
+        if (target_time >= start_time[i] and target_time <= end_time[i]):
+            result += 1
+
+    return result
